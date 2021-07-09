0083d9646658a35d105964b998b819f04412cf14
pedrodev777 <tobiasspedro594@gmail.com>
2021-07-09 19:11:17 -0300

    first commit

---
+++ events.js
@@ -0,0 +1,28 @@
+class EventEmitter {
+  constructor() {
+    
+    this._events = new Map();
+    
+  }
+  
+  on(event, listiner) {
+    
+    if(typeof event !== 'string') throw new TypeError('First argument must be a string.');
+    
+    if(typeof listiner !== 'function') throw new TypeError('second argument must be a function.');
+    
+    return this._events.set(event, listiner);
+    
+  }
+  
+  emit(event, ...args) {
+    
+    if(!this._events.has(event)) return null;
+    
+    return this._events.get(event)
+    .call(this, args);
+    
+  }
+}
+
+module.exports = EventEmitter;
---
+++ readme.md
@@ -0,0 +1,24 @@
+# @areojs/events
+
+```javascript
+const EventEmitter = require("@areojs/events");
+
+const Events = new EventEmitter();
+
+Events.on('newMessage', async message => {
+  
+  console.log(`${message.user.name}: ${message.content}`);
+});
+
+/* ... */
+
+Events.emit('newMessage',
+   {
+     user: {
+       name: 'Wz'
+     },
+     content: 'Hello guys!'
+   });
+   
+/* Wz: hello guys! */
+```
