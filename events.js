19d8ab80ab0cb0131e1115ff5082a5f0f137e03d
Wz <63757378+DablioZe@users.noreply.github.com>
2021-07-09 21:16:55 -0300

    areojs/events

--- events.js
+++ events.js
@@ -1,27 +1,19 @@
 class EventEmitter {
   constructor() {
-    
-    this._events = new Map();
-    
+    this._events = new Map(); 
   }
   
-  on(event, listiner) {
-    
+  on(event, listener) {
     if(typeof event !== 'string') throw new TypeError('First argument must be a string.');
-    
-    if(typeof listiner !== 'function') throw new TypeError('second argument must be a function.');
-    
-    return this._events.set(event, listiner);
+    if(typeof listener !== 'function') throw new TypeError('second argument must be a function.');
+    return this._events.set(event, listener);
     
   }
   
   emit(event, ...args) {
-    
     if(!this._events.has(event)) return null;
-    
     return this._events.get(event)
     .apply(this, args);
-    
   }
 }
 
